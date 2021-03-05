550758629074e7cf2c0f2e044c2991d8eda08eb4
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-05 13:11:34 -0500

    have expression parser setup for testing as I go

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Text.Parsec as P
 main :: IO ()
 main = do
   putStrLn "Enter some input to parse:"
-  result <- P.parse parseExpr "" <$> getLine
+  result <- P.parse expression "" <$> getLine
   print result
 
 
