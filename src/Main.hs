1cd665956b5a3cab13ec74d155e5b2d46e3b05e4
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-12 20:41:25 -0500

    now parsing files as input

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,19 @@
 module Main where
 
 import Parser
+import Syntax
 
 import Text.Parsec as P
+
+handleNodes :: Either ParseError Program -> IO ()
+handleNodes (Left err) = error $ show err
+handleNodes (Right ns) = do
+  print ns
+  mconcat <$> mapM print ns
   
 main :: IO ()
 main = do
-  putStrLn "Enter some input to parse:"
-  result <- P.parse expression "" <$> getLine
-  print result
-
+  fname <- getLine
+  nodes <- P.parse program fname <$> readFile fname
+  handleNodes nodes
 
