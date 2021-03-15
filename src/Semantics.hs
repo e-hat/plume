093be251118852835b10d6b73e8613ff12111b07
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-15 17:38:06 -0400

    now generates global symbol table without typechecking

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -1,13 +1,35 @@
 module Semantics where
 
 import qualified Data.Map.Strict as Map
+import Text.Printf (errorShortFormat, printf)
 
 import Syntax
 
-data Symbol = Var Identifier | Func Identifier [Type] 
+data Symbol = Var Identifier | Func Identifier [Type] deriving (Eq, Ord, Show)
 
-newtype SymTable = SymTable { getTable :: Map.Map Symbol Type } 
+type SymTable = Map.Map Symbol Type
 
-getDeclSymbol :: Decl -> (Symbol, Type)
-getDeclSymbol (Let t i _) = (Var i, t)
-getDeclSymbol (DefFn i ps r _) = (Func i (map (fst . getParam) ps), r)
+semanticErr :: Node t -> String -> a
+semanticErr (Node s c) msg = error $ printf "%s: %s" (show s) msg
+
+getDeclSymbol :: Decl -> Symbol
+getDeclSymbol (Let _ i _) = Var i
+getDeclSymbol (DefFn i ps _ _) = Func i (map (fst . getParam) ps)
+
+getDeclType :: Decl -> Type
+getDeclType (Let t _ _) = t
+getDeclType (DefFn _ _ r _) = r
+getDeclType _ = "Void"
+
+genGlobalSyms :: [DeclNode] -> SymTable
+genGlobalSyms [d] =  
+  Map.singleton (getDeclSymbol $ getContent d) (getDeclType $ getContent d)
+genGlobalSyms (d:ds) = 
+  let 
+    rest = genGlobalSyms ds
+    sym  = getDeclSymbol $ getContent d
+  in
+    case Map.lookup sym rest of
+      Nothing -> Map.insert sym (getDeclType $ getContent d) rest
+      Just _  -> semanticErr d "symbols cannot share name in global scope"
+
