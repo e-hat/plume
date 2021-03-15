386f090664610e9fb21622cb8d0164b88673bcba
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-15 10:47:40 -0400

    added command line args

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,45 @@ module Main where
 
 import Parser
 import Syntax
-import Text.Parsec as P
-import Text.Show.Pretty
 
-handleNodes :: Either ParseError Program -> IO ()
+import qualified Text.Parsec as P
+import Text.Show.Pretty
+import Options.Applicative
+
+import Data.Semigroup ((<>))
+
+handleNodes :: Either P.ParseError Program -> IO ()
 handleNodes (Left err) = error $ show err
 handleNodes (Right ns) = dumpIO ns
 
+data CLInput = CLInput 
+  { filename :: String 
+  , printAST :: Bool }
+
+clinput :: Parser CLInput
+clinput = CLInput <$>
+  argument str 
+    ( metavar "FILE" 
+   <> help "Plume source file to be compiled" )
+  <*> switch 
+    ( long "print-ast"
+   <> short 'a'
+   <> help "Print the AST generated for this Plume source file" 
+   <> showDefault )
+
 main :: IO ()
-main = do
-  fname <- getLine
-  nodes <- P.parse program fname <$> readFile fname
-  handleNodes nodes
+main = run =<< execParser opts
+  where
+    opts = info (clinput <**> helper)
+      ( fullDesc
+     <> progDesc "Compile a Plume source file"
+     <> header "Plume - strongly, statically typed programming language" )
+
+run :: CLInput -> IO ()
+run (CLInput f True) = do
+  nodes <- P.parse program f <$> readFile f
+  case nodes of 
+    Left err -> error $ show err
+    Right ast -> dumpIO ast
+
+run (CLInput _ False) = undefined
