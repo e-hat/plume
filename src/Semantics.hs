2282b187f2ff5b566c78b165a12c9be24fc6eea3
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-16 11:20:18 -0400

    added comments + infrastructure to begin validation

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -1,6 +1,7 @@
 module Semantics where
 
 import qualified Data.Map.Strict as Map
+
 import SemanticError
 import Syntax
 
@@ -15,6 +16,8 @@ type SymTable = Map.Map Symbol Type
 getDeclSymbol :: Decl -> Symbol
 getDeclSymbol (Let _ i _) = Var i
 getDeclSymbol (DefFn i ps _ _) = Func i (map (fst . getParam) ps)
+-- other types of declarations are anonymous
+getDeclSymbol _ = undefined
 
 -- gets the type for a let or function definition declaration
 getDeclType :: Decl -> Type
@@ -24,14 +27,57 @@ getDeclType _ = "Void"
 
 -- generates a global list of symbols
 -- I do NOT want to have forward declaration be a thing
-genGlobalSyms :: [DeclNode] -> SymTable
-genGlobalSyms = genGlobalSymsImpl . reverse
+-- reverses input list so that error message shows up for the first
+-- occurence of an overlap
+-- this function returns a list of *unique* symbols from the global scope
+genGlobalSyms :: Program -> SymTable
+genGlobalSyms = genGlobalSymsImpl . reverse . getProgram
   where
-    genGlobalSymsImpl [d] = 
+    genGlobalSymsImpl :: [DeclNode] -> SymTable
+    genGlobalSymsImpl [d] =
       Map.singleton (getDeclSymbol $ getContent d) (getDeclType $ getContent d)
     genGlobalSymsImpl (d : ds) =
       let rest = genGlobalSymsImpl ds
           sym = getDeclSymbol $ getContent d
-      in case Map.lookup sym rest of
-        Nothing -> Map.insert sym (getDeclType $ getContent d) rest
-        Just _ -> semanticErr d "cannot have overlapping symbol declarations in global scope"
+       in case Map.lookup sym rest of
+            Nothing -> Map.insert sym (getDeclType $ getContent d) rest
+            Just _ -> semanticErr d "cannot have overlapping symbol declarations in global scope"
+
+data SymTableTree = SymTableTree
+  { getParent :: Maybe SymTableTree,
+    getTable :: SymTable,
+    getChildren :: [SymTableTree]
+  }
+
+lookupStt :: Symbol -> SymTableTree -> Maybe Type
+lookupStt sym (SymTableTree p t _) = case Map.lookup sym t of
+                                    Nothing -> case p of 
+                                                 Nothing -> Nothing 
+                                                 Just stt -> lookupStt sym stt
+                                    Just t -> Just t
+
+class Validated a where
+  validate :: a -> SymTableTree -> Either String SymTableTree
+
+-- slightly ugly setup, doing this because a Node t either fails and exits or 
+-- succeeds, whereas a Validated t either fails with a msg or succeeds
+class Checkable a where
+  check :: a -> SymTableTree -> SymTableTree
+
+instance (ErrRep t, Validated t) => Checkable (Node t) where
+  check n@(Node sr c) base = case validate c base of
+                             Left msg -> semanticErr n msg
+                             Right result -> result
+
+instance Validated Decl where
+  validate d base = undefined
+
+instance Validated Expr where
+  validate e base = undefined
+
+validateProgram :: Program -> SymTableTree
+validateProgram p = 
+  let baseTree = SymTableTree Nothing (genGlobalSyms p) [] 
+  in 
+    foldr check baseTree (getProgram p) 
+  
