1d4dfe5ce27634ebe6ec22a9a076399fec4af388
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-20 12:57:35 -0400

    added buildGlobalScops that...builds the globalScopes in prep for semantics

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -6,3 +6,18 @@ import Data.Maybe
 import SemanticError
 import SymbolTable
 import Syntax
+
+buildGlobalScopes :: Program -> [(Decl SpanRec, SymData)]
+buildGlobalScopes (Program astdaugs) = buildGlobalScopesImpl (map getASTDeclAug astdaugs)
+  where
+    buildGlobalScopesImpl :: [DeclAug SpanRec] -> [(Decl SpanRec, SymData)]
+    buildGlobalScopesImpl ds = 
+      let addIfAbsent :: DeclAug SpanRec -> SymTable -> SymTable 
+          addIfAbsent entry tbl = 
+            case Map.lookup (getDeclSymbol entry) tbl of
+              Just _ -> astSemanticErr entry ("symbol " ++ getDeclSymbol entry ++ " has already been declared in global scope")
+              Nothing -> insertDecl entry tbl
+          symMap = foldr addIfAbsent Map.empty ds
+          buildEntry :: DeclAug SpanRec -> (Decl SpanRec, SymData)
+          buildEntry (d, sr) = (d, SymData symMap sr)
+      in map buildEntry ds 
