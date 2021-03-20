5a27c1be5426de28fdaf07827369da8819c6f9a4
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-03-20 16:12:05 -0400

    tentative structure for type checking and scope building

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -1,4 +1,4 @@
-module Semantics where
+module Semantics (validateSemantics) where
 
 import Data.List
 import qualified Data.Map.Strict as Map
@@ -7,17 +7,30 @@ import SemanticError
 import SymbolTable
 import Syntax
 
-buildGlobalScopes :: Program -> [(Decl SpanRec, SymData)]
-buildGlobalScopes (Program astdaugs) = buildGlobalScopesImpl (map getASTDeclAug astdaugs)
-  where
-    buildGlobalScopesImpl :: [DeclAug SpanRec] -> [(Decl SpanRec, SymData)]
-    buildGlobalScopesImpl ds = 
-      let addIfAbsent :: DeclAug SpanRec -> SymTable -> SymTable 
-          addIfAbsent entry tbl = 
-            case Map.lookup (getDeclSymbol entry) tbl of
-              Just _ -> astSemanticErr entry ("symbol " ++ getDeclSymbol entry ++ " has already been declared in global scope")
-              Nothing -> insertDecl entry tbl
-          symMap = foldr addIfAbsent Map.empty ds
-          buildEntry :: DeclAug SpanRec -> (Decl SpanRec, SymData)
-          buildEntry (d, sr) = (d, SymData symMap sr)
-      in map buildEntry ds 
+validateSemantics :: Program -> [SymDeclAug]
+validateSemantics p =
+  let globals = map getASTDeclAug (getProgram p)
+      globalScope = buildGlobalScope globals
+      symTrees = map (buildSymTreeD globalScope) globals
+   in map (SymDeclAug . typecheckD) symTrees
+
+buildGlobalScope :: [DeclAug SpanRec] -> SymTable
+buildGlobalScope ds =
+  let addIfAbsent :: DeclAug SpanRec -> SymTable -> SymTable
+      addIfAbsent entry tbl =
+        case Map.lookup (getDeclSymbol entry) tbl of
+          Just _ -> astSemanticErr entry ("symbol " ++ getDeclSymbol entry ++ " has already been declared in global scope")
+          Nothing -> insertDecl entry tbl
+   in foldr addIfAbsent Map.empty ds
+
+buildSymTreeD :: SymTable -> DeclAug SpanRec -> DeclAug SymData
+buildSymTreeD = undefined
+
+buildSymTreeE :: SymTable -> ExprAug SpanRec -> ExprAug SymData
+buildSymTreeE = undefined
+
+typecheckD :: DeclAug SymData -> DeclAug SymData
+typecheckD = undefined
+
+getType :: ExprAug SymData -> Type
+getType = undefined
