2cb884c7a99ac795e6c9a20781075921d406889f
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-07 11:51:59 -0400

    fixed bug with more than one label at a line number

--- src/ShowBytecode.hs
+++ src/ShowBytecode.hs
@@ -8,14 +8,17 @@ import Text.Printf (errorShortFormat, printf)
 instance Show BytecodeProgram where
   show (BytecodeProgram is tbl) =
     let is' = map ((++ "\n") . show) is
-        ftbl = M.fromList $ map swap (M.toList tbl)
+        ftbl = map swap (M.toList tbl)
         fmtInst :: Integer -> String -> String
         fmtInst ln i' = printf "%08d@\t%s" ln i'
         zipNums :: Integer -> String -> String
         zipNums ln i' =
-          case M.lookup ln ftbl of
-            Just l -> printf "%s:\n%s" l (fmtInst ln i')
-            Nothing -> fmtInst ln i'
+          case map snd (filter ((== ln) . fst) ftbl) of
+            l:ls  -> 
+              printf "%s:\n" l ++
+                concatMap (printf "%s:\n") ls ++
+                  fmtInst ln i'
+            [] -> fmtInst ln i'
      in concat $ zipWith zipNums [1 ..] is'
 
 instance Show Value where
