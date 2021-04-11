fd4383caa1024ad498e89f4872f07c4a6f23e0e6
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-11 13:44:10 -0400

    formatted

--- src/BytecodeGen.hs
+++ src/BytecodeGen.hs
@@ -1,8 +1,8 @@
 module BytecodeGen where
 
+import Control.Monad.State
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
-import Control.Monad.State
 import SymbolTable
 import Syntax
 
@@ -20,31 +20,31 @@ data BytecodeProgram = BytecodeProgram
     getLabelTable :: M.Map String Integer
   }
 
-data GState = GState 
+data GState = GState
   { getCurrentProgram :: BytecodeProgram,
     getOpenRegisters :: [Integer],
-    getVarRegisters :: M.Map String Integer, 
+    getVarRegisters :: M.Map String Integer,
     getGlobalVars :: S.Set String
   }
 
 setCurrentProgram :: BytecodeProgram -> State GState ()
-setCurrentProgram b = modify $ \s -> s { getCurrentProgram = b }
+setCurrentProgram b = modify $ \s -> s {getCurrentProgram = b}
 
-setOpenRegisters :: [Integer] -> State GState () 
-setOpenRegisters rs = modify $ \s -> s { getOpenRegisters = rs }
+setOpenRegisters :: [Integer] -> State GState ()
+setOpenRegisters rs = modify $ \s -> s {getOpenRegisters = rs}
 
-setVarRegisters :: M.Map String Integer -> State GState () 
-setVarRegisters vr = modify $ \s -> s { getVarRegisters = vr }
+setVarRegisters :: M.Map String Integer -> State GState ()
+setVarRegisters vr = modify $ \s -> s {getVarRegisters = vr}
 
 setGlobalVars :: S.Set String -> State GState ()
-setGlobalVars gv = modify $ \s -> s { getGlobalVars = gv }
+setGlobalVars gv = modify $ \s -> s {getGlobalVars = gv}
 
-initState :: GState 
-initState = GState (BytecodeProgram [] M.empty) [1..] M.empty S.empty
+initState :: GState
+initState = GState (BytecodeProgram [] M.empty) [1 ..] M.empty S.empty
 
 genBytecode :: SymTreeList -> BytecodeProgram
-genBytecode trees = 
-  let statefulProgram = 
+genBytecode trees =
+  let statefulProgram =
         traverse (genGlobalTree . getSymDeclAug) (getSymTreeList trees)
       (_, result) = runState statefulProgram initState
    in getCurrentProgram result
