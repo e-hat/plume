23dd9aaa28dce8b0699b2ff682d3af374afe0964
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-11 23:43:35 -0400

    VM is now stateful and ready to handle more insts

--- src/VirtualMachine.hs
+++ src/VirtualMachine.hs
@@ -2,12 +2,51 @@ module VirtualMachine where
 
 import BytecodeGen
 import System.Exit
+import Control.Monad.State
+import qualified Data.Map.Strict as M
+
+data VMState = VMState 
+  { getRegisters :: M.Map Integer Value,
+    getCurrentProgram :: BytecodeProgram,
+    getIPtr :: Integer,
+    getRunning :: Bool
+  } 
+
+setRegisters :: M.Map Integer Value -> State VMState ()
+setRegisters new = modify $ \s -> s { getRegisters = new }
+
+setRegister :: Integer -> Value -> State VMState ()
+setRegister r v = do 
+  s <- get 
+  let cur = getRegisters s 
+  setRegisters (M.insert r v cur)
 
 runBytecode :: BytecodeProgram -> IO ()
-runBytecode b@(BytecodeProgram is tbl) = mapM_ (runInst b) is
+runBytecode b@(BytecodeProgram is tbl) = 
+  let start = tbl M.! "main"
+      (rslt, _) = runState (runFrom start) (VMState M.empty b start True)
+   in rslt
 
-runInst :: BytecodeProgram -> Inst -> IO ()
-runInst _ (Ret (VInt v)) = case v of
-  0 -> exitSuccess
-  _ -> exitWith $ ExitFailure (fromIntegral v)
-runInst _ _ = error "haven't implemented this yet"
+runFrom :: Integer -> State VMState (IO ())
+runFrom start = do
+  s <- get
+  put $ s { getIPtr = start }
+  result <- 
+    runInst $ getInstructions (getCurrentProgram s) !! fromIntegral (start - 1)
+  s' <- get
+  if getRunning s' 
+    then if getIPtr s' /= start 
+             then runFrom $ getIPtr s' 
+             else runFrom $ start + 1
+    else return result
+
+runInst :: Inst -> State VMState (IO ())
+runInst (Move v (Register r)) = do 
+  setRegister r v
+  return (pure ())
+runInst Ret = do
+  s <- get 
+  put $ s { getRunning = False }
+  case getRegisters s M.! retReg of 
+    (VInt 0) -> return exitSuccess 
+    (VInt v) -> return $ exitWith $ ExitFailure (fromIntegral v)
