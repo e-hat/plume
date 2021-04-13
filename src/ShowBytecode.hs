387b418c798a67cf4c40f288a8b0e33d4fee0d4f
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-13 10:58:36 -0400

    add Show instance for Add inst

--- src/ShowBytecode.hs
+++ src/ShowBytecode.hs
@@ -31,3 +31,4 @@ instance Show Value where
 instance Show Inst where
   show (Move v r) = printf "Mov %s -> %s" (show v) (show r)
   show Ret = "Ret"
+  show (Add v1 v2 r) = printf "Add (%s, %s) -> %s" (show v1) (show v2) (show r)
