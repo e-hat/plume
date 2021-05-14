64925c1b22187a41f52f888e389ae2123c55601f
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-05-14 09:30:00 -0400

    Add function for retrieving mapping from functions to list of insts

--- src/LiveIntervals.hs
+++ src/LiveIntervals.hs
@@ -2,6 +2,7 @@ module LiveIntervals (liveIntervals) where
 
 import BytecodeGen (Inst (..), BytecodeProgram (..), Label (..))
 import qualified Data.Map.Strict as M
+import Data.List
 
 type LiveInterval = (Integer, Integer)
 
@@ -10,10 +11,23 @@ type LiveInterval = (Integer, Integer)
 liveIntervals :: BytecodeProgram -> M.Map Label [LiveInterval]
 liveIntervals = undefined
 
-getFuncLines :: BytecodeProgram -> [Integer]
-getFuncLines bp = M.elems $ M.filterWithKey isFuncLbl lt 
+funcs :: BytecodeProgram -> M.Map Label [Inst]
+funcs b = M.fromList $ 
+  zip (map fst flocs) (sliceLocs (map (fromInteger . snd) flocs) is)
+    where 
+      is = getInstructions b
+      flbls = funcLbls b
+      flocs = sortBy (\(_, a) (_, b) -> compare a b) (M.assocs flbls)
+
+funcLbls :: BytecodeProgram -> M.Map Label Integer
+funcLbls b = M.filterWithKey isFuncLbl lt 
   where 
-    lt = getLabelTable bp
+    lt = getLabelTable b
     isFuncLbl :: Label -> Integer -> Bool
     isFuncLbl (FuncLabel _) _ = True 
     isFuncLbl _ _ = False
+
+sliceLocs :: [Int] -> [b] -> [[b]]
+sliceLocs [i] bs = [drop i bs]
+sliceLocs (beg:end:as) bs =
+  take (end - beg) (drop beg bs) : sliceLocs (end : as) bs
