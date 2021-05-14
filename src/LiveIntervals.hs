49e44fba286207697a18f1981da0ab4a7eef2be5
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-05-13 20:49:44 -0400

    Identify function locations from Label Table

--- src/LiveIntervals.hs
+++ src/LiveIntervals.hs
@@ -1,8 +1,18 @@
 module LiveIntervals (liveIntervals) where
 
-import BytecodeGen (Inst (..))
+import BytecodeGen (Inst (..), BytecodeProgram (..), Label (..))
+import qualified Data.Map.Strict as M
 
 type LiveInterval = (Integer, Integer)
 
 liveIntervals :: [Inst] -> [LiveInterval]
 liveIntervals = undefined
+
+getFuncLines :: BytecodeProgram -> [Integer]
+getFuncLines bp = M.elems $ M.filterWithKey isFuncLbl lt 
+  where 
+    lt = getLabelTable bp
+    isFuncLbl :: Label -> Integer -> Bool
+    isFuncLbl (FuncLabel _) _ = True 
+    isFuncLbl _ _ = False
+    
