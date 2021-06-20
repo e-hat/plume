d8d472e4dca13e40df5a6a9b5ded71e900ae2d2a
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-06-19 18:10:41 -0600

    Added RegAlloc cli

--- src/Main.hs
+++ src/Main.hs
@@ -1,124 +1,143 @@
 module Main where
 
 import Bytecode.Generation
-import Bytecode.Types()
+import Bytecode.Types ()
 import Parsing.Parser
-import Parsing.Syntax()
+import Parsing.Syntax ()
 import RegAlloc.LiveIntervals
 import Semantics.Validation
+import RegAlloc.LinearScan
 
-import Control.Monad()
-import Data.Semigroup()
+import Control.Monad ()
+import Data.Semigroup ()
 import Options.Applicative
 import System.IO
 import qualified Text.Parsec as P
 import Text.Show.Pretty
 
 data Input
-    = ASTInput String
-    | ValInput String
-    | RunInput String
-    | PrintBytecodeInput String
-    | LiveIntervalInput String
+  = ASTInput String
+  | ValInput String
+  | RunInput String
+  | PrintBytecodeInput String
+  | LiveIntervalInput String
+  | RegAllocInput String
 
 astInput :: Parser Input
 astInput =
-    ASTInput
-        <$> strOption
-            ( long "print-ast"
-                <> short 'a'
-                <> metavar "FILENAME"
-                <> help "Print the AST of the input file"
-            )
+  ASTInput
+    <$> strOption
+      ( long "print-ast"
+          <> short 'a'
+          <> metavar "FILENAME"
+          <> help "Print the AST of the input file"
+      )
 
 valInput :: Parser Input
 valInput =
-    ValInput
-        <$> strOption
-            ( long "validate-sem"
-                <> short 'v'
-                <> metavar "FILENAME"
-                <> help "Validate the semantics of the input file"
-            )
+  ValInput
+    <$> strOption
+      ( long "validate-sem"
+          <> short 'v'
+          <> metavar "FILENAME"
+          <> help "Validate the semantics of the input file"
+      )
 
 printBytecodeInput :: Parser Input
 printBytecodeInput =
-    PrintBytecodeInput
-        <$> strOption
-            ( long "print-bytecode"
-                <> short 'b'
-                <> metavar "FILENAME"
-                <> help "Print the bytecode generated for the input file"
-            )
+  PrintBytecodeInput
+    <$> strOption
+      ( long "print-bytecode"
+          <> short 'b'
+          <> metavar "FILENAME"
+          <> help "Print the bytecode generated for the input file"
+      )
 
 liveIntervalInput :: Parser Input
 liveIntervalInput =
-    LiveIntervalInput
-        <$> strOption
-            ( long "live-intervals"
-                <> short 'l'
-                <> metavar "FILENAME"
-                <> help "Display the virtual registers' live intervals for the bytecode generated from the input file"
-            )
+  LiveIntervalInput
+    <$> strOption
+      ( long "live-intervals"
+          <> short 'l'
+          <> metavar "FILENAME"
+          <> help "Display the virtual registers' live intervals for the bytecode generated from the input file"
+      )
+
+regallocInput :: Parser Input 
+regallocInput = 
+  RegAllocInput 
+    <$> strOption 
+      ( long "regalloc"
+          <> short 'r'
+          <> metavar "FILENAME" 
+          <> help "Print the bytecode after performing register allocation"
+      )
 
 compileOptions :: Parser Input
-compileOptions = astInput <|> valInput <|> printBytecodeInput <|> liveIntervalInput
+compileOptions = astInput <|> valInput <|> printBytecodeInput <|> liveIntervalInput <|> regallocInput
 
 runArg :: Parser Input
 runArg =
-    RunInput <$> argument str (metavar "FILE")
+  RunInput <$> argument str (metavar "FILE")
 
 input :: Parser Input
 input =
-    hsubparser
-        ( command
-            "compile"
-            ( info
-                (compileOptions <**> helper)
-                (progDesc "Compile a Plume source file")
-            )
-            <> command
-                "run"
-                ( info
-                    (runArg <**> helper)
-                    (progDesc "Run a Plume program")
-                )
+  hsubparser
+    ( command
+        "compile"
+        ( info
+            (compileOptions <**> helper)
+            (progDesc "Compile a Plume source file")
         )
+        <> command
+          "run"
+          ( info
+              (runArg <**> helper)
+              (progDesc "Run a Plume program")
+          )
+    )
 
 main :: IO ()
 main = run =<< execParser opts
-  where
-    opts =
-        info
-            (input <**> helper)
-            (progDesc "Plume - strongly, statically typed programming language")
+ where
+  opts =
+    info
+      (input <**> helper)
+      (progDesc "Plume - strongly, statically typed programming language")
 
 run :: Input -> IO ()
 run (ASTInput f) = do
-    nodes <- P.parse program f <$> readFile f
-    case nodes of
-        Left err -> print err
-        Right p -> dumpIO p
+  nodes <- P.parse program f <$> readFile f
+  case nodes of
+    Left err -> print err
+    Right p -> dumpIO p
 run (ValInput f) = do
-    nodes <- P.parse program f <$> readFile f
-    case nodes of
-        Left err -> print err
-        Right p -> case validateSemantics p of
-            Left err -> putStrLn err
-            Right _ -> putStrLn ("Validation of " ++ f ++ " successful.")
+  nodes <- P.parse program f <$> readFile f
+  case nodes of
+    Left err -> print err
+    Right p -> case validateSemantics p of
+      Left err -> putStrLn err
+      Right _ -> putStrLn ("Validation of " ++ f ++ " successful.")
 run (PrintBytecodeInput f) = do
-    nodes <- P.parse program f <$> readFile f
-    case nodes of
-        Left err -> print err
-        Right p -> case validateSemantics p of
-            Left err -> putStrLn err
-            Right trees -> print $ genBytecode trees
+  nodes <- P.parse program f <$> readFile f
+  case nodes of
+    Left err -> print err
+    Right p -> case validateSemantics p of
+      Left err -> putStrLn err
+      Right trees -> print $ genBytecode trees
 run RunInput{} = do
-    hPutStrLn stderr "The Plume VM has been deprecated and is no longer available for use."
+  hPutStrLn stderr "The Plume VM has been deprecated and is no longer available for use."
 run (LiveIntervalInput f) = do
-    nodes <- P.parse program f <$> readFile f
-    case nodes of
-        Left err -> print err
-        Right p -> case validateSemantics p of
-            Left err -> putStrLn err
-            Right trees -> print $ liveIntervals $ genBytecode trees
+  nodes <- P.parse program f <$> readFile f
+  case nodes of
+    Left err -> print err
+    Right p -> case validateSemantics p of
+      Left err -> putStrLn err
+      Right trees -> print $ liveIntervals $ genBytecode trees
+run (RegAllocInput f) = do
+  nodes <- P.parse program f <$> readFile f 
+  case nodes of  
+    Left err -> print err 
+    Right p -> case validateSemantics p of 
+      Left err -> putStrLn err 
+      Right trees -> print $ regalloc $ genBytecode trees
