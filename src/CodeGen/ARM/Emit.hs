0e4e39a4214585dc7a6a8ac852d0ac47998f932f
Edward Hatfield <edward.hatfield@tufts.edu>
2022-10-09 12:33:55 -0400

    Use Writer monad for emitting ARM asm
    Also add a little more CodeGen. You can now write Plume programs that
    print the meaning of life!

--- src/CodeGen/ARM/Emit.hs
+++ src/CodeGen/ARM/Emit.hs
@@ -1,43 +1,66 @@
 module CodeGen.ARM.Emit where
 
 import CodeGen.ARM.RegAlloc
-import Data.List
+
+import Control.Monad.Writer
 import qualified Data.Map.Strict as M
 import qualified Ir.Tac.Types as T
 import Text.Printf
 
 emit :: Program -> String
-emit p = programPrologue ++ functionDefinitions
+emit p = execWriter $ programPrologue >> functionDefinitions
   where
-    step :: String -> String -> Func -> String
-    step accum "main" f = accum ++ "\n.global main\nmain:\n" ++ emitFunction f
-    step accum name f = accum ++ "\n" ++ name ++ ":\n" ++ emitFunction f
-    functionDefinitions = M.foldlWithKey step "" $ getProgram p
+    step :: String -> Func -> Writer String ()
+    step "main" f = do
+        tell "\n.global main\nmain:\n"
+        emitFunction f
+    step name f = do
+        tell $ "\n" ++ name ++ ":\n"
+        emitFunction f
+    functionDefinitions :: Writer String ()
+    functionDefinitions = mapM_ (uncurry step) (M.assocs (getProgram p))
 
-programPrologue :: String
-programPrologue = ".text"
+programPrologue :: Writer String ()
+programPrologue = do tell ".text"
 
-emitFunction :: Func -> String
-emitFunction (Func _ _ insts stackSize) = functionPrologue stackSize ++ intercalate "\n" (map emitInst insts)
+emitFunction :: Func -> Writer String ()
+emitFunction (Func _ _ insts stackSize) = functionPrologue stackSize >> mapM_ emitInst insts
 
-functionPrologue :: Int -> String
-functionPrologue stackSize =
-    "  push {fp, lr}\n"
-        ++ "  mov fp, sp\n"
-        ++ "  push {r0, r1, r2, r3}\n"
-        ++ printf "  sub sp, sp, #%d\n" alignedStackOffset
+functionPrologue :: Int -> Writer String ()
+functionPrologue stackSize = do
+    tell $
+        "  // prologue\n"
+            ++ "  push {fp, lr}\n"
+            ++ "  mov fp, sp\n"
+            ++ "  push {r0, r1, r2, r3}\n"
+            ++ printf "  // total aligned stack size: %d bytes\n" (nextAlignedByte stackSize)
+            ++ printf "  sub sp, sp, #%d\n\n" alignedStackOffset
   where
-    alignedStackOffset = nextAlignedByte 8 $ stackSize - 16
+    alignedStackOffset = nextAlignedByte $ stackSize - 16
 
-functionEpilogue :: String
-functionEpilogue = "  mov sp, fp\n" ++ "  pop {fp, pc}"
+functionEpilogue :: Writer String ()
+functionEpilogue = do tell $ "  // epilogue\n" ++ "  mov sp, fp\n" ++ "  pop {fp, pc}\n"
 
-emitInst :: Inst -> String
+emitInst :: Inst -> Writer String ()
 emitInst (T.Return Nothing) = functionEpilogue
-emitInst _ = ""
+emitInst (T.Return (Just (T.None t))) = do
+    tell
+        ( case t of
+            (MemoryLoc _ Stack{}) -> "  str r0, "
+            _ -> "  mov r0, "
+        )
+    emitTerm t
+    tell "\n"
+    functionEpilogue
+emitInst _ = do tell ""
 
-nextAlignedByte :: Int -> Int -> Int
-nextAlignedByte alignment n =
-    if n `mod` alignment == 0
+emitTerm :: Term -> Writer String ()
+emitTerm (MemoryLoc _ (Register n)) = do tell $ "r" ++ show n
+emitTerm (MemoryLoc _ (Stack n)) = do tell $ printf "[fp, #-%d]" n
+emitTerm (Immediate _ n) = do tell $ "#" ++ show n
+
+nextAlignedByte :: Int -> Int
+nextAlignedByte n =
+    if n `mod` 8 == 0
         then n
-        else (n `div` alignment + 1) * alignment
+        else (n `div` 8 + 1) * 8
--- src/CodeGen/ARM/RegAlloc.hs
+++ src/CodeGen/ARM/RegAlloc.hs
@@ -9,11 +9,11 @@ import Text.Printf
 
 type Type = String
 
-data Memory = Register Int | Stack Int | ParamRegister Int | Global Int
+data Memory = Register Int | Stack Int
 
 data Term
-    = MemoryLoc Type Memory
-    | Immediate Type Int32
+  = MemoryLoc Type Memory
+  | Immediate Type Int32
 
 type Inst = T.GeneralInst Term
 
@@ -22,23 +22,21 @@ data Func = Func {getParamTypes :: [Type], getReturnType :: Type, getFunc :: [In
 newtype Program = Program {getProgram :: M.Map String Func}
 
 instance Show Memory where
-    show (Register n) = "r" ++ show n
-    show (Stack n) = printf "[fp, #-%s]" (show n)
-    show (ParamRegister n) = "p" ++ show n
-    show (Global n) = "g" ++ show n
+  show (Register n) = "r" ++ show n
+  show (Stack n) = printf "[fp, #-%s]" (show n)
 
 instance Show Term where
-    show (MemoryLoc _ m) = show m
-    show (Immediate _ n) = printf "0x%08x" n
+  show (MemoryLoc _ m) = show m
+  show (Immediate _ n) = printf "0x%08x" n
 
 instance Show Func where
-    show = intercalate "\n" . concatMap T.showInst . getFunc
+  show = intercalate "\n" . concatMap T.showInst . getFunc
 
 instance Show Program where
-    show = intercalate "\n\n" . map step . M.assocs . getProgram
-      where
-        step :: (String, Func) -> String
-        step (fname, f) = printf "define %s\n%s" fname (show f)
+  show = intercalate "\n\n" . map step . M.assocs . getProgram
+   where
+    step :: (String, Func) -> String
+    step (fname, f) = printf "define %s\n%s" fname (show f)
 
 -- Spills every single variable
 naiveRegAlloc :: T.Program -> Program
@@ -46,21 +44,21 @@ naiveRegAlloc = Program . M.map naiveRegAllocFunc . T.getProgram
 
 naiveRegAllocFunc :: T.Func -> Func
 naiveRegAllocFunc (T.Func paramTypes returnType insts) =
-    let paramToStackLoc = M.fromList $ map (\i -> ("p" ++ show i, 16 - 4 * i)) (take (length paramTypes) [0 ..] :: [Int])
-        (highestLoc, localToStackLoc) = foldl (foldl step) (20, M.empty) insts
-        step :: (Int, M.Map String Int) -> T.Term -> (Int, M.Map String Int)
-        step (nextStackLoc, m) (T.Subs (T.Local localId _)) =
-            if M.member ("l" ++ show localId) m
-                then (nextStackLoc, m)
-                else (nextStackLoc + 4, M.insert ("l" ++ show localId) nextStackLoc m)
-        step m _ = m
-        stackLocs = M.union paramToStackLoc localToStackLoc
-     in Func paramTypes returnType (map (fmap (replaceWithStackRead stackLocs)) insts) highestLoc
+  let paramToStackLoc = M.fromList $ map (\i -> ("p" ++ show i, 16 - 4 * i)) (take (length paramTypes) [0 ..] :: [Int])
+      (highestLoc, localToStackLoc) = foldl (foldl step) (20, M.empty) insts
+      step :: (Int, M.Map String Int) -> T.Term -> (Int, M.Map String Int)
+      step (nextStackLoc, m) (T.Subs (T.Local localId _)) =
+        if M.member ("l" ++ show localId) m
+          then (nextStackLoc, m)
+          else (nextStackLoc + 4, M.insert ("l" ++ show localId) nextStackLoc m)
+      step m _ = m
+      stackLocs = M.union paramToStackLoc localToStackLoc
+   in Func paramTypes returnType (map (fmap (replaceWithStackRead stackLocs)) insts) highestLoc
 
 replaceWithStackRead :: M.Map String Int -> T.Term -> Term
 replaceWithStackRead m (T.Subs (T.Param paramId typ)) = MemoryLoc typ $ Stack $ m M.! ("p" ++ show paramId)
 replaceWithStackRead m (T.Subs (T.Local localId typ)) = MemoryLoc typ $ Stack $ m M.! ("l" ++ show localId)
-replaceWithStackRead _ (T.Subs (T.Global globalId typ)) = MemoryLoc typ $ Global globalId
+replaceWithStackRead _ (T.Subs (T.Global globalId typ)) = undefined
 replaceWithStackRead _ (T.LitInt n) = Immediate "Int" (fromInteger n)
 replaceWithStackRead _ (T.LitFloat n) = Immediate "Float" ((decode $ encode n) :: Int32)
 replaceWithStackRead _ (T.LitString{}) = Immediate "String" 0x0
